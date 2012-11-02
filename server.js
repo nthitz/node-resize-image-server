03a1fcf0297913edc9f9ef384182d92b89468f80
Philmod <philippe.modard@gmail.com>
2012-11-02 20:38:06 +0100

    get, store, and send an image

--- server.js
+++ server.js
@@ -1,19 +1,57 @@
 /**
  * Module dependencies.
  */
-var express = require('express');
+var express = require('express')
+	, getImg  = require('./lib/getImg.js')
+	, transfImg  = require('./lib/transfImg.js')
+	, fs = require('fs')
+	;
+
+/**
+ * Options.
+ */
+var img_path = __dirname + '\\img\\';
 
 /**
  * Express server.
  */
 var app = express();
 
+
 /**
  * Express routes.
  */
 app.get('/', function(req, res){
-  res.send('Hello World');
+	var u = req.query.u // URL
+		, w = req.query.w // width
+		, h = req.query.h // height
+		, filename = u.split('/').slice(-1).toString()
+		;
+	var options = {
+			url: u
+		, dest: img_path + filename
+	}
+
+	getImg.loadImg(options, function(err,image) {
+		if (err) res.send(500);
+		else {
+			fs.readFile(image, 'binary', function(err,data) {
+				res.writeHead(200, {"Content-Type": "image/png"});
+	  		res.end(data, "binary");
+	  	});
+			/*transfImg.resize(image,w,h,function(err,image) {
+				if (err) res.send(500);
+				else {
+					console.log('image = ' + image.length);
+					res.writeHead(200, {"Content-Type": "image/png"});
+      		res.end(image, "binary");
+				}
+			});*/
+		}
+	});
+
 });
+
 
 app.listen(3000, function() {
 	console.log("Express server listening on port %d in %s mode", app.settings.port || 3000, app.settings.env);
